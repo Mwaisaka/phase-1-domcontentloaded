d43c6527ef0022b5410e6429a66c324999bf97d3
Frank <frankkashere@gmail.com>
2023-10-23 20:15:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function(){
+    console.log("The DOM content has been loaded");
+    const p=document.querySelector('p#text');
+    p.textContent = "This is really cool!";
+    p.appendChild(p.textContent);
+});
+
